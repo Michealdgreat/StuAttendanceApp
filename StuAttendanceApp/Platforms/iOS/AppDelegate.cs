cb8469ef69843c1f31441ccefb615b17b9a59a77
MICHEAL SHODAMOLA <143539396+Michealdgreat@users.noreply.github.com>
2024-11-16 14:07:06 +0200

    Project files clean up

--- StuAttendanceApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace StuAttendanceApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- StuAttendanceApp/ViewModels/WelcomeViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StuAttendanceApp.ViewModels;
-
-public partial class WelcomeViewModel : BaseViewModel
-{
-	[ObservableProperty]
-	public string source = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-}
