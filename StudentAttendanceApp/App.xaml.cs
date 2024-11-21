c9de6370a3cf3044c2b0d1818dfe15fec9f7de0a
Micheal Shodamola <michealshodamola01@gmail.com>
2024-11-21 20:21:49 +0200

    Add Form ENtry Helper class and Implementation.

--- StudentAttendanceApp/App.xaml.cs
+++ StudentAttendanceApp/App.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace StudentAttendanceApp
+﻿using StudentAttendanceApp.Services;
+
+namespace StudentAttendanceApp
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            #region FORM HELPER
+            //Borderless editor
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(FormHelper), (handler, view) =>
+            {
+                if (view is FormHelper)
+                {
+#if __ANDROID__
+                handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif __IOS__
+                    handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif __WINDOWS__
+                handler.PlatformView.FontWeight = Microsoft.UI.Text.FontWeights.Thin;                
+                handler.PlatformView.BorderThickness = new Thickness(0);
+#endif
+                }
+            });
+            #endregion
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
