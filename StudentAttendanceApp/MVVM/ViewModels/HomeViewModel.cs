c60e84e58df2ec7dd20cac7f56d614a5cd34f776
Micheal Shodamola <michealshodamola01@gmail.com>
2024-11-24 18:53:17 +0200

    Update Home page & Viewmodel, Login and register page. Home and Login page looks cool now.

--- StudentAttendanceApp/MVVM/ViewModels/HomeViewModel.cs
+++ StudentAttendanceApp/MVVM/ViewModels/HomeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using StudentAttendanceApp.MVVM.ViewModels.Base;
+using StudentAttendanceApp.MVVM.Views;
 using System.ComponentModel;
 
 namespace StudentAttendanceApp.MVVM.ViewModels
@@ -8,5 +9,18 @@ namespace StudentAttendanceApp.MVVM.ViewModels
     public partial class HomeViewModel : BaseViewModel
     {
 
+
+
+        [RelayCommand]
+        public async Task LoginYourAccount()
+        {
+            await Shell.Current.GoToAsync(nameof(LoginPage));
+        }
+
+        [RelayCommand]
+        public async Task RegisterForAnAccount()
+        {
+            await Shell.Current.GoToAsync(nameof(RegisterPage));
+        }
     }
 }
