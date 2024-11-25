13978d700d8e50f5b7c4421fbd879ad500e7bc82
Micheal Shodamola <michealshodamola01@gmail.com>
2024-11-25 22:37:27 +0200

    Add IndexPage (as tab 0 root page). Update Profile page look and feel.

--- StudentAttendanceApp/MVVM/ViewModels/LoginViewModel.cs
+++ StudentAttendanceApp/MVVM/ViewModels/LoginViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using StudentAttendanceApp.MVVM.ViewModels.Base;
+using StudentAttendanceApp.MVVM.Views;
+using StudentAttendanceApp.Services;
 using System.ComponentModel;
 
 namespace StudentAttendanceApp.MVVM.ViewModels
 {
-    public partial class LoginViewModel : BaseViewModel
+    public partial class LoginViewModel() : BaseViewModel
     {
 
+        [RelayCommand]
+        public async Task LoginButton()
+        {
+            var commonService = MauiProgram.ServiceProvider!.GetService<CommonService>();
+
+            commonService?.InitializeAppShell();
+            await Shell.Current.GoToAsync($"///{nameof(ProfilePage)}", true);
+        }
     }
 }
